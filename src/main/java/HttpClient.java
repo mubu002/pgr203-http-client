696d05241248bfb880be695e67fd59179f4568f3
mubu002@student.kristiania.no <mubu002@student.kristiania.no>
2021-09-22 16:08:13 +0200

    make a HTTP connection to the server

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -1,5 +1,20 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
 public class HttpClient {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("httpbin.org", 80);
+        String request = ("GET /html HTTP/1.1\r\n" +
+                "Host: httpbin.org\r\n" +
+                "\r\n");
+        socket.getOutputStream().write(request.getBytes());
+
+        InputStream in = socket.getInputStream();
+        int c;
+         while((c = in.read()) != -1){
+            System.out.print((char)c);
+        }
+
     }
 }
