02603f9c36f5146fc043282d0b4e97cb0cc82881
mubu002@student.kristiania.no <mubu002@student.kristiania.no>
2021-09-22 18:02:35 +0200

    Add initial test

---
+++ src/test/java/HttpClientTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpClientTest {
+    @Test
+    void dummyTest(){
+        assertEquals(35, 7*5);
+    }
+
+}
