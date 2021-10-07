6471053c80203570f73fed64cb85b2f465dbbe04
mubu002@student.kristiania.no <mubu002@student.kristiania.no>
2021-10-07 13:03:43 +0200

    added time'out minutes

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -1,29 +1,82 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class HttpClient {
 
-    public HttpClient(String host, int port, String requestTarget) {
+    private final int statusCode;
+    private final HashMap<String, String> headerFields = new HashMap<>();
+    private final String getMessageBody;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
+        String request = ("GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Connection: close\r\n" +
+                "Host: " + host + "\r\n" +
+                "\r\n");
+        socket.getOutputStream().write(request.getBytes());
+        String statusLine = readLine(socket);
+        this.statusCode = Integer.parseInt(statusLine.split(" ")[1]);
+
+        String  headerLine;
+        while(!(headerLine = readLine(socket)).isBlank()){
+          int colonPos =  headerLine.indexOf(':');
+          String key = headerLine.substring(0, colonPos);
+          String value = headerLine.substring(colonPos+1).trim();
+          headerFields.put(key, value);
+
+        }
+
+        this.getMessageBody = readCharacters(socket, getContentLength());
+
+    }
+
+    private String readCharacters(Socket socket, int contentLength) throws IOException {
+        InputStream in = socket.getInputStream();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < contentLength; i++) {
+            result.append((char) in.read());
+        }
+
+
+        return result.toString();
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        InputStream in = socket.getInputStream();
+        StringBuilder result = new StringBuilder();
+
+
+
+        int c;
+        while((c = in.read()) != -1 && c != '\r'){
+            result.append((char)c);
+        }
+        in.read();
+        return result.toString();
     }
 
 
     public int getStatusCode(){
-        return 200;
+        return statusCode;
+    }
+
+    public String getHeader(String s) {
+        return headerFields.get(s);
     }
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("httpbin.org", 80);
-        String request = ("GET /html HTTP/1.1\r\n" +
-                "Connection: close\r\r" +
-                "Host: httpbin.org\r\n" +
-                "\r\n");
-        socket.getOutputStream().write(request.getBytes());
 
-        InputStream in = socket.getInputStream();
-        int c;
-         while((c = in.read()) != -1){
-            System.out.print((char)c);
-        }
+    }
+
+
+    public int getContentLength() {
+        return Integer.parseInt(getHeader("Content-Length"));
+    }
+
+    public String getMessageBody(){
+        return this.getMessageBody;
 
     }
 }
--- src/test/java/HttpClientTest.java
+++ src/test/java/HttpClientTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpClientTest {
@@ -8,15 +10,33 @@ class HttpClientTest {
         assertEquals(35, 7*5);
     }
     @Test
-    void shouldGet200ResponseCode() {
+    void shouldGet200ResponseCode() throws IOException {
         HttpClient client = new HttpClient("httpbin.org", 80, "/html");
         assertEquals(200, client.getStatusCode());
     }
     @Test
-    void shouldGet401ResponseCode() {
-        HttpClient client = new HttpClient("httpbin.org", 80, "/status/403");
+    void shouldGet401ResponseCode() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/status/401");
         assertEquals(401, client.getStatusCode());
     }
 
+    @Test
+    void shouldReadResponseHeaders() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        assertEquals("text/html; charset=utf-8", client.getHeader("Content-Type"));
+    }
+    @Test
+    void shouldReadContentLength() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        assertEquals(3741, client.getContentLength());
 
+    }
+    @Test
+    void shouldReadMessageBody() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        assertTrue(
+                client.getMessageBody().startsWith("<!DOCTYPE html>\n<html"),
+                "Should be html: " + client.getMessageBody()
+        );
+    }
 }
